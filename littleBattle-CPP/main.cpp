dc0eb8c7d9862d909252cd9bc74c7c1b5b019b98
kelvin knigton <keltheceo@gmail.com>
2016-06-17 10:29:20 -0700

    1st commit

---
+++ littleBattle-CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  littleBattle-CPP
+//
+//  Created by Kelvin Knighton on 6/16/16.
+//  Copyright © 2016 Kelvin Knighton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
